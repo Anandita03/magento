9a52a6606aa28ee6e6ff6f2255935a716534c957
Anandita03 <anandita.bhui@conversionbug.com>
2018-07-12 18:51:21 +0530

    new commit

--- app/code/Conversionbug/HelloMagento/Controller/Index/Event.php
+++ app/code/Conversionbug/HelloMagento/Controller/Index/Event.php
@@ -7,9 +7,29 @@ class Event extends \Magento\Framework\App\Action\Action
 
 	public function execute()
 	{
-		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Conversionbug'));
-		$this->_eventManager->dispatch('conversionbug_hellomagento_display_text', ['cb_text' => $textDisplay]);
-		echo $textDisplay->getText();
-		exit;
+		// $textDisplay = new \Magento\Framework\DataObject(array('text' => 'Conversionbug'));
+		// $this->_eventManager->dispatch('conversionbug_hellomagento_display_text', ['cb_text' => $textDisplay]);
+		// echo $textDisplay->getText();
+		// exit;
+
+		// $textDisplay = new \Magento\Framework\DataObject();
+		// $this->_eventManager->dispatch('customer_login');
+		// echo $textDisplay->getText();
+		// exit;
+
+
+		// $textDisplay = new \Magento\Framework\DataObject();
+		// $this->_eventManager->dispatch('checkout_onepage_controller_success_action', ['order_ids' => [$session->getLastOrderId()]]);
+		// echo $textDisplay->getText();
+		// exit;
+
+
+		$this->_eventManager->dispatch('catalog_product_save_after');
+
+		// echo 'YY';exit;
+		// $textDisplay = new \Magento\Framework\DataObject();
+		// $this->_eventManager->dispatch('customer_logout', ['customer' => $this->getCustomer()]);
+		// echo $textDisplay->getText();
+		// exit;
 	}
 }
--- app/code/Conversionbug/HelloMagento/Observer/ChangeDisplayText.php
+++ app/code/Conversionbug/HelloMagento/Observer/ChangeDisplayText.php
@@ -4,12 +4,90 @@ namespace Conversionbug\HelloMagento\Observer;
 
 class ChangeDisplayText implements \Magento\Framework\Event\ObserverInterface
 {
-	public function execute(\Magento\Framework\Event\Observer $observer)
-	{
-		$displayText = $observer->getData('cb_text');
-		echo $displayText->getText() . " - Event </br>";
-		$displayText->setText('Execute event successfully.');
+	// protected $_order;
+	// public function __construct(
+	// 	\Magento\Sales\Api\Data\OrderInterface $order
+    // ) {
+	// 	 $this->_order = $order; 
+		   
+    // }
 
-		return $this;
+	// public function execute(\Magento\Framework\Event\Observer $observer)
+	// {
+		// $displayText = $observer->getData('cb_text');
+		// echo $displayText->getText() . " - Event </br>";
+		// $displayText->setText('Execute event successfully.');
+
+
+	//Displaying the name of customer when he/she logins
+		// echo "Customer LoggedIn<br/>";
+        // $customer = $observer->getEvent()->getCustomer();
+        // echo $customer->getName(); //Get customer name
+        // exit;
+		// return $this;
+
+		// echo "Order Placed<br/>";
+		// $order = $observer->getEvent()->getOrder();
+		// // $quote = $observer->getQuote();
+		// echo "order";
+
+	// protected $logger;  
+    // public function __construct(        
+    //     \Psr\Log\LoggerInterface $logger
+    // ) {
+    //     $this->logger = $logger;       
+    // }
+ 
+	public function execute(\Magento\Framework\Event\Observer $observer)
+	{       
+		
+		// $orderids = $observer->getEvent()->getOrderIds();
+
+        // foreach($orderids as $orderid){
+        //     $order = $this->_order->load($orderid);
+        // }
+		$product = $observer->getEvent()->getProduct();
+
+
+		// $pro = $observer->getProduct()->getName();
+		// $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/debug.log');
+		// $logger = new \Zend\Log\Logger();
+		// $logger->addWriter($writer);
+		// $logger->info($pro->getName());
+
+    //     $order = $observer->getEvent()->getOrder();
+    // // $order_id = $order->getID();
+    // $order_number = $order->getIncrementId();
+    // foreach ($order->getAllItems() as $item) {
+    //         $ProdustIds[] = $item->getProductId();
+    //         $ProdustSku[] = $item->getSku();
+    //         $proName[] = $item->getName(); //product name
+    //         $this->_logger->addDebug('Item Name: ' . $item->getName() . 'Item ID: ' . $item->getProductId());
+    //     }
+        
+			// $logger->info(print_r($order->getOrder(), true));
+        // if (count($orderIds)) {
+        //     $orderId = $orderIds[0];            
+        //     $order = $this->_orderFactory->create()->load($orderId);
+        //     $shippingAddress = $order->getShippingAddress();
+        //     echo "Order";
+		// 	$this->logger->debug('Logging HelloWorld Observer'); 
+		
+	//Displaying the name of customer when he/she logins
+		// $customer = $observer->getEvent()->getCustomer();	
+		// $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/debug.log');
+		// $logger = new \Zend\Log\Logger();
+		// $logger->addWriter($writer);
+		// $logger->info($customer->getName());
+
+		// $this->logger->debug('Customer logout');
+		// $this->_logger->info('Customer logout');
+
+		// echo "Order";
+		// 	$quote = $observer->getEvent()->getQuote();
+		// 	if ($quote->getIsCheckoutCart()) {
+		// 	$a = $this->checkoutSession->getQuoteId($quote->getId());
+		// 		echo $a;
+		// 	}
 	}
-}
+	}
