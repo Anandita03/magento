46fe9c4d8f7df3effa8a4fbc91753aeab6373e56
Anandita03 <anandita.bhui@conversionbug.com>
2018-07-20 13:59:53 +0530

    controller task

---
+++ app/code/Conversionbug/Contact/Block/ContactForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Conversionbug\Contact\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class ContactForm extends \Magento\Contact\Block\ContactForm
+{
+    /**
+     * @param Template\Context $context
+     * @param array $data
+     */
+    public function __construct(Template\Context $context, array $data = [])
+    {
+        parent::__construct($context, $data);
+        $this->_isScopePrivate = true;
+    }
+
+    public function getFormAction()
+    {
+       // echo "override block";
+        return $this->getUrl('contact/index/post', ['_secure' => true]);
+    }
+
+    // public function getText() 
+    // { 
+    //     // $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/debug.log');
+	//  	// $logger = new \Zend\Log\Logger();
+	//  	// $logger->addWriter($writer);
+    //  return "Override Text"; 
+    // }
+}
