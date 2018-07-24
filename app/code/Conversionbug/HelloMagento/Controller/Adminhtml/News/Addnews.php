f0bf8f264fabb3db449b2b967dcfebf593d24a76
Anandita03 <anandita.bhui@conversionbug.com>
2018-07-24 20:11:14 +0530

    system config commit

---
+++ app/code/Conversionbug/HelloMagento/Controller/Adminhtml/News/Addnews.php
@@ -0,0 +1,12 @@
+<?php
+ 
+namespace Conversionbug\HelloMagento\Controller\Adminhtml\News; 
+
+class Addnews extends \Magento\Backend\App\Action
+{
+
+    public function execute()
+    {
+        echo "Add News Section";
+    }
+}
---
+++ app/code/Conversionbug/HelloMagento/Controller/Adminhtml/News/Index.php
@@ -0,0 +1,12 @@
+<?php
+ 
+namespace Conversionbug\HelloMagento\Controller\Adminhtml\News; 
+
+class Index extends \Magento\Backend\App\Action
+{
+
+    public function execute()
+    {
+        echo "Manage News Section";
+    }
+}
